45f733babc28ba0485ee7848bc170b7d76dedf3b
mdevelopeur <mdevelopeur@gmail.com>
2026-02-27 17:53:03 +0500

    Update functions.py

--- api/functions.py
+++ api/functions.py
@@ -1,1 +1,19 @@
 
+
+
+async def new_order_handler(data):
+
+
+async def get_products(client, last_id):
+  url = mpfit + ""
+  body = {
+  "limit": 200,
+  "last_id": last_id
+  }
+  result = await client.post(url=url, headers=mpfit_headers, json=body)
+  result = result.json()
+  #products = result["data"]
+  if len(result["data"]) == 200:
+    products = await get_products(client, result["last_id"])
+    products.extend(result["data"])
+  return products
